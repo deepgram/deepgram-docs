06aca14d933e55b6e9a4321bc3f71be64509f5ad
Luke Oliff <luke@lukeoliff.com>
2025-03-08 13:19:56 +0000

    feat: styled docsearch for deepgram

--- fern/assets/component.js
+++ fern/assets/component.js
@@ -3,6 +3,26 @@
 //   heap.load("765739241");
 // </script>
 
+
+function deepgramGlobalStyles() {
+  const styles = document.createElement("style");
+  styles.innerHTML = `
+:root {
+  --deepgram-primary: #13ef95;
+  --deepgram-secondary: #201cff;
+  --deepgram-background: #0b0b0c;
+  --deepgram-modal-background: #1A1A1F;
+  --deepgram-translucent-background: rgba(0, 0, 0, 0.5);
+  --deepgram-header-background: #1A1A1F;
+  --deepgram-sidebar-background: #0B0B0C;
+  --deepgram-card-background: #1A1A1F;
+  --deepgram-border: #2C2C33;
+  --deepgram-text: #fbfbff;
+  --deepgram-muted: #949498;
+}`;
+  document.head.appendChild(styles);
+}
+
 function insertHeap() {
   const script = document.createElement("script");
   script.type = "text/javascript";
@@ -65,6 +85,92 @@ async function startAlgolia() {
   link.href = "https://cdn.jsdelivr.net/npm/@docsearch/css@3.9.0/dist/style.min.css";
   document.head.appendChild(link);
 
+  const styles = document.createElement("style");
+  styles.innerHTML = `
+:root {
+  --docsearch-primary-color: var(--deepgram-primary);
+  --docsearch-text-color: var(--deepgram-text);
+  --docsearch-spacing: 12px;
+  --docsearch-icon-stroke-width: 1.4;
+  --docsearch-highlight-color: var(--docsearch-primary-color);
+  --docsearch-muted-color: var(--deepgram-muted);
+  --docsearch-container-background: var(--deepgram-translucent-background);
+  --docsearch-logo-color: var(--deepgram-primary);
+
+  /* modal */
+  --docsearch-modal-width: 720px;
+  --docsearch-modal-height: 800px;
+  --docsearch-modal-background: var(--deepgram-modal-background);
+  --docsearch-modal-shadow: inset 1px 1px 0 0 var(--deepgram-translucent-background),
+    0 3px 8px 0 var(--deepgram-background);
+
+  /* searchbox */
+  --docsearch-searchbox-height: 56px;
+  --docsearch-searchbox-background: var(--deepgram-background);
+  --docsearch-searchbox-focus-background: var(--deepgram-background);
+  --docsearch-searchbox-shadow: inset 0 0 0 2px var(--docsearch-primary-color);
+
+  /* hit */
+  --docsearch-hit-height: 56px;
+  --docsearch-hit-color: var(--deepgram-text);
+  --docsearch-hit-active-color: var(--deepgram-background);
+  --docsearch-hit-background: var(--deepgram-background);
+  --docsearch-hit-shadow: 0 1px 3px 0 var(--deepgram-modal-background);
+
+  /* key */
+  --docsearch-key-gradient: linear-gradient(
+    -225deg,
+    var(--deepgram-deepgram-modal-background) 0%,
+    var(--deepgram-background) 100%
+  );
+  --docsearch-key-shadow: inset 0 -2px 0 0 var(--deepgram-deepgram-modal-background),
+    inset 0 0 1px 1px var(--deepgram-deepgram-modal-background),
+    0 1px 2px 1px var(--deepgram-translucent-background);
+  --docsearch-key-pressed-shadow: inset 0 -2px 0 0 var(--deepgram-deepgram-modal-background),
+    inset 0 0 1px 1px var(--deepgram-deepgram-modal-background),
+    0 1px 1px 0 var(--deepgram-translucent-background);
+
+  /* footer */
+  --docsearch-footer-height: 44px;
+  --docsearch-footer-background: var(--deepgram-modal-background);
+  --docsearch-footer-shadow: 0 -1px 0 0 var(--deepgram-border),
+    0 -3px 6px 0 var(--deepgram-translucent-background);
+}
+
+/* Darkmode */
+
+html[data-theme="dark"] {
+  --docsearch-text-color: var(--deepgram-text);
+  --docsearch-container-background: var(--deepgram-translucent-background);
+  --docsearch-modal-background: var(--deepgram-modal-background);
+  --docsearch-modal-shadow: inset 1px 1px 0 0 var(--deepgram-translucent-background),
+    0 3px 8px 0 var(--deepgram-background);
+  --docsearch-searchbox-background: var(--deepgram-background);
+  --docsearch-searchbox-focus-background: var(--deepgram-background);
+  --docsearch-hit-color: var(--deepgram-text);
+  --docsearch-hit-background: var(--deepgram-background);
+  --docsearch-hit-shadow: 0 1px 3px 0 var(--deepgram-modal-background);
+  --docsearch-key-gradient: linear-gradient(
+    -225deg,
+    var(--deepgram-deepgram-modal-background) 0%,
+    var(--deepgram-background) 100%
+  );
+  --docsearch-key-shadow: inset 0 -2px 0 0 var(--deepgram-deepgram-modal-background),
+    inset 0 0 1px 1px var(--deepgram-deepgram-modal-background),
+    0 1px 2px 1px var(--deepgram-translucent-background);
+  --docsearch-key-pressed-shadow: inset 0 -2px 0 0 var(--deepgram-deepgram-modal-background),
+    inset 0 0 1px 1px var(--deepgram-deepgram-modal-background),
+    0 1px 1px 0 var(--deepgram-translucent-background);
+  --docsearch-footer-background: var(--deepgram-modal-background);
+  --docsearch-footer-shadow: 0 -1px 0 0 var(--deepgram-border),
+    0 -3px 6px 0 var(--deepgram-translucent-background);
+  --docsearch-muted-color: var(--deepgram-muted);
+  --docsearch-logo-color: var(--deepgram-primary);
+}
+
+  `;
+  document.head.appendChild(styles);
+
   const { default: docsearch } = await import(
     "https://cdn.jsdelivr.net/npm/@docsearch/js@3.9.0/+esm"
   );
@@ -73,10 +179,27 @@ async function startAlgolia() {
   const indexName = "crawler_unified";
   const apiKey = "e50ef768d9ac1a2b80ac6101639df429";
 
-  docsearch({ container: ".fern-header-searchbar", appId, indexName, apiKey });
+  const mobileSearch = document.createElement("span");
+  mobileSearch.id = "mobile-search";
+
+  const desktopSearch = document.createElement("span");
+  desktopSearch.id = "desktop-search";
+
+  const fernMobileSearchButton = document.querySelector(".fern-header-right-menu #fern-search-button");
+  fernMobileSearchButton.parentNode.insertBefore(mobileSearch, fernMobileSearchButton);
+
+  const fernDesktopSearchButton = document.querySelector(".fern-header-searchbar #fern-search-button");
+  fernDesktopSearchButton.parentNode.insertBefore(desktopSearch, fernDesktopSearchButton);
+
+  fernMobileSearchButton.remove();
+  fernDesktopSearchButton.remove();
+
+  docsearch({ container: "#desktop-search", appId, indexName, apiKey });
+  docsearch({ container: "#mobile-search", appId, indexName, apiKey });
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
+  deepgramGlobalStyles();
   insertKapaWidget();
   await startAlgolia();
   insertHeap();
@@ -86,6 +209,7 @@ if (
   document.readyState === "complete" ||
   document.readyState === "interactive"
 ) {
+  deepgramGlobalStyles();
   insertKapaWidget();
   await startAlgolia();
   insertHeap();
